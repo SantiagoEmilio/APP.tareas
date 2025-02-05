3e8e8e7ba3d543992af3734cc89febef6a2e4224
Santiago <slopez@scl.edu.gt>
2025-02-04 20:00:48 -0600

    header actualizado

--- componentes/header/header.js
+++ componentes/header/header.js
@@ -1,6 +1,17 @@
 function header() {
     let header = document.createElement('header');
     header.className = "div-header";
+
+    let logo = document.createElement('img');
+    logo.src = "https://w7.pngwing.com/pngs/514/837/png-transparent-snoopy-illustration-snoopy-charlie-brown-wood-peanuts-snoopy-miscellaneous-white-text-thumbnail.png";
+    header.appendChild(logo);
+
+    let h1 = document.createElement('h1');
+    h1.innerText = "pokis";
+    header.appendChild(h1);
+
     return header;
+
+
 }
 export { header };
